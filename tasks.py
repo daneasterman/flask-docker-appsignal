86690fff71c11010186c3a2315facb8328e0c2b2
Daniel Easterman <daniel.easterman@gmail.com>
2025-04-22 16:51:21 +0100

    use env var in tasks.py

--- tasks.py
+++ tasks.py
@@ -1,7 +1,9 @@
 import time
+import os
 from celery import Celery
 
-celery_app = Celery('tasks', broker='amqp://guest:guest@rabbitmq:5672//')
+broker_url = os.environ.get("CELERY_BROKER_URL", "amqp://guest:guest@rabbitmq:5672//")
+celery_app = Celery('tasks', broker=broker_url)
 
 @celery_app.task
 def generate_report():
