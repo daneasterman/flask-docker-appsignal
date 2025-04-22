536fe9a6f0960f6701951f4be63a19293f6a3554
Daniel Easterman <daniel.easterman@gmail.com>
2025-04-22 17:39:18 +0100

    Add startCommand to render.yaml

--- tasks.py
+++ tasks.py
@@ -7,8 +7,6 @@ load_dotenv()
 broker_url = os.environ.get("CELERY_BROKER_URL", "amqp://guest:guest@rabbitmq:5672//")
 celery_app = Celery('tasks', broker=broker_url)
 
-print("**BROKER_URL**", broker_url)
-
 @celery_app.task
 def generate_report():
     time.sleep(5)
